69dff4ce1e0cb50413159833f775b5aa20966113
Evan Kallas <144737982+egkallas@users.noreply.github.com>
2024-04-15 12:32:23 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,13 @@
 function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
+    if (n <= 1) return 1;
+    if (n == 2) return 2;
+    var fact = 2;
+    var j = fact + 1;
+    while (j <= n) {
+        fact = fact * j
+        j++
+    }
+    return fact;
 }
 
 function e(n) {
