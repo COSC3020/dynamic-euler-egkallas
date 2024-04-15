4f21cf1fd33035403af54a17d27576e266ea35b5
Evan Kallas <144737982+egkallas@users.noreply.github.com>
2024-04-15 12:32:42 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,4 +3,14 @@ const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
 assert(factorial(10) === 3628800);
+assert(factorial(0) === 1);
+assert(factorial(1) === 1);
+assert(factorial(5) === 120);
+assert(factorial(7) === 5040);
+
+
 assert(e(10) === 2.7182818011463845);
+assert(e(0) === 1);
+assert(e(5) === 2.7166666666666663);
+assert(e(1000) === 2.7182818284590455);
+assert(e(666) === 2.7182818284590455);
